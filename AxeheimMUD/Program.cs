d68c75970721cbb45e7550216121f3c9981979b4
Aiden Arnkels-Webb <aiden.webb@gmail.com>
2022-08-16 09:48:36 +0100

    Add basic instantiation and control flow

--- AxeheimMUD/Program.cs
+++ AxeheimMUD/Program.cs
@@ -3,14 +3,32 @@ namespace AxeheimMUD
 {
     internal class Program
     {
+        static Random random = new Random();
+
         static void Main(string[] args)
         {
-            
-            Room room = new Room();
+            List<Room> rooms = new List<Room>();
+            for (int i = 0; i < random.Next(100); i++)
+            {
+                rooms.Add(new Room());
+            }
 
-            Console.WriteLine(room.DescribeRoom());
-            
-            
+            foreach (Room room in rooms)
+            {
+                Console.WriteLine(room.DescribeRoom());
+                Console.WriteLine(room.RoomId);
+            }
+
+            Player player = new Player();
+
+            while (player.Health > 0)
+            {
+                // Prompt player to take an action
+                player.PromptForAction();
+                
+            }
+
+
         }
     }
 }
