7c73264e6511a633c1b2686aed5297421536b4ad
Aiden Arnkels-Webb <aiden.webb@gmail.com>
2022-08-16 09:48:08 +0100

    Add PlayerAction bindings and basic movement handling

--- AxeheimMUD/Models/Player.cs
+++ AxeheimMUD/Models/Player.cs
@@ -11,5 +11,43 @@ namespace AxeheimMUD.Models
         public int Health { get; set; }
         public int Strength { get; set; }
         public int Position { get; set; }
+        public List<PlayerAction> ValidActions = new List<PlayerAction>();
+
+        public Player()
+        {
+            Health = 100;
+            Strength = 1;
+            Position = 0;
+            _initialiseActions();
+        }
+        
+        public void _initialiseActions()
+        {
+            ValidActions.Add(new PlayerAction(EntityAction.MoveNorth, 'N'));
+            ValidActions.Add(new PlayerAction(EntityAction.MoveEast, 'E'));
+            ValidActions.Add(new PlayerAction(EntityAction.MoveSouth, 'S'));
+            ValidActions.Add(new PlayerAction(EntityAction.MoveWest, 'W'));
+            ValidActions.Add(new PlayerAction(EntityAction.MoveUp, 'U'));
+            ValidActions.Add(new PlayerAction(EntityAction.MoveDown, 'D'));
+        }
+
+        public EntityAction PromptForAction()
+        {
+            while (true)
+            {
+                Console.Write("Choose an action: ");
+                char keychar = Console.ReadKey(false).KeyChar;
+                if (ValidActions.Any(x => x.KeyBinding.Equals(keychar)))
+                {
+                    EntityAction entityAction = ValidActions.First(x => x.KeyBinding.Equals(keychar)).EntityAction;
+                    Console.WriteLine($" - {entityAction}");
+                    return entityAction;
+                }
+                else
+                {
+                    Console.Error.WriteLine($" - {keychar.ToString()} is not a valid action");
+                }
+            }
+        }
     }
 }
---
+++ AxeheimMUD/Models/PlayerAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxeheimMUD.Models
+{
+    internal class PlayerAction
+    {
+        public EntityAction EntityAction { get; set; }
+        public char KeyBinding { get; set; }
+
+        public PlayerAction(EntityAction entityAction, char keyBinding)
+        {
+            EntityAction = entityAction;
+            KeyBinding = keyBinding;
+        }
+    }
+
+    internal enum EntityAction
+    {
+        MoveNorth,
+        MoveEast,
+        MoveSouth,
+        MoveWest,
+        MoveUp,
+        MoveDown,
+    }
+}
