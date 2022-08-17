ae6902cd114a9285597e5f2ceb94ab200f0cfa58
Aiden Arnkels-Webb <aiden.webb@gmail.com>
2022-08-17 13:51:43 +0100

    Add beginnings of TCP server listener

---
+++ AxeheimMUD/Server/TcpServerListener.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.IO;
+
+namespace AxeheimMUD.Server
+{
+    internal class TcpServerListener
+    {
+        private static int _port = 9090;
+        private static IPAddress _localAddress = IPAddress.Parse("127.0.0.1");
+
+        private TcpListener _server = new TcpListener(_localAddress, _port);
+        
+    }
+}
